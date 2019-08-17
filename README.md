b28fedeab2c91022136f4b3e1f422c88ba07d22c
Aravind Chowdary <46242119+KAraviChow@users.noreply.github.com>
2019-08-17 23:04:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EasyImageEditor-Android
+ 
---
+++ app/src/main/java/com/aravi/easyeditor/tools/ToolType.java
@@ -0,0 +1,10 @@
+package com.aravi.easyeditor.tools;
+
+public enum ToolType {
+    BRUSH,
+    TEXT,
+    ERASER,
+    FILTER,
+    EMOJI,
+    STICKER
+}
